f86912ab1d327f04b1909063f1d13b6465b231a3
Alex Dean <github@mostlyalex.com>
2017-01-26 08:42:07 -0600

    support query by LCCN numbers also
    sometimes handy when ISBN search fails

--- sru_fetch.rb
+++ sru_fetch.rb
@@ -34,8 +34,25 @@ db.execute <<-EOF
   )
 EOF
 
-def find_and_write_marc(isbn, log, db)
+# build an SRU query from the input
+#
+# inputs are assumed to be isbn-ish (maybe EAN, etc) number
+# unless they begin with 'lccn:', in which case they are considered to be
+# Library of Congress control numbers.
+#
+# @return [String] a query fragment usable on an SRU server like lx2.loc.gov
+def build_sru_query(input)
+  if input[0..4] == 'lccn:'
+    "bath.lccn=#{input[5..-1]}"
+  else
+    "bath.isbn=#{input}"
+  end
+end
+
+def find_and_write_marc(input, log, db)
   begin
+    log.info "#{input} : start."
+
     base_query = {
       version: '1.1',
       operation: 'searchRetrieve',
@@ -43,17 +60,19 @@ def find_and_write_marc(isbn, log, db)
       maximumRecords: 1
     }
 
-    log.info "#{isbn} : start."
-
     response = RestClient.get('http://lx2.loc.gov:210/lcdb',
-      params: base_query.merge(query: "bath.isbn=#{isbn}")
+      params: base_query.merge(query: build_sru_query(input))
     )
     document = Nokogiri::XML(response.body)
     document.remove_namespaces!
 
     if document.xpath('//numberOfRecords').text.to_i == 0
-      log.warn "\a#{isbn} : not found.".colorize(:light_red)
-      db.query("REPLACE INTO unknowns (id, last_scanned_at) VALUES (?, ?)", isbn, Time.now.utc.iso8601)
+      log.warn "\a#{input} : not found.".colorize(:light_red)
+      db.query(
+        "REPLACE INTO unknowns (id, last_scanned_at) VALUES (?, ?)",
+        input,
+        Time.now.utc.iso8601
+      )
       return
     end
 
@@ -67,20 +86,20 @@ def find_and_write_marc(isbn, log, db)
     # http://www.loc.gov/marc/bibliographic/bd59x.html
     marc << MARC::DataField.new('590', '0', '0',
       # the UPC/EAN/ISBN that we actually scanned, so it can be used for searches
-      ['a', isbn],
+      ['a', input],
       # and the URL that we retrieved it from
       ['b', response.request.url],
       ['c', Time.now.utc.iso8601]
     )
 
-    writer = MARC::Writer.new("out/#{isbn}.marc")
+    writer = MARC::Writer.new("out/#{input.gsub(':', '-')}.marc")
     writer.write(marc)
     writer.close
 
     # print the title. http://www.loc.gov/marc/bibliographic/bd20x24x.html
-    log.info "\a#{isbn} : finished. '#{marc['245']['a']}'".colorize(:light_green)
+    log.info "\a#{input} : finished. '#{marc['245']['a']}'".colorize(:light_green)
   rescue => e
-    log.error "\a#{isbn} : #{e.class} #{e.message} ".colorize(:light_red)
+    log.error "\a#{input} : #{e.class} #{e.message} #{e.backtrace.join("\n")}".colorize(:light_red)
   end
 end
 
@@ -99,6 +118,6 @@ loop do
     no_echo = !no_echo
     log.info "echo #{no_echo ? 'off' : 'on'}"
   else
-    Thread.new { find_and_write_marc(isbn, log, db) }
+    Thread.new { find_and_write_marc(input, log, db) }
   end
 end
